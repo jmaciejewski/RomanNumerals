5df97c34a7c38ddc48622f71e21e8110217af6ea
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 22:38:15 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# RomanNumerals
+# RomanNumeral
---
+++ src/main/java/RomanNumeral.java
@@ -0,0 +1,14 @@
+public class RomanNumeral {
+    int number;
+    String romanNumerical;
+    public RomanNumeral(int number){}
+    public String getRomanNumeral()
+    {
+        return this.romanNumerical;
+    }
+
+    public int getNumber()
+    {
+        return this.number;
+    }
+}
